b0588ad45737dac27a9065140199efcf92b158eb
shisen <sl943@cornell.edu>
2012-11-27 23:28:48 -0500

    Revert "still nothing.."
    This reverts commit 2a099d429906c4590a25341df234282f232867ed.

--- test2.py
+++
@@ -1,7 +0,0 @@
-from framework import *
-import pickle
-from UnbiasedNet import *
-
-net=UnbiasedNet.NeuralNet(1,1,1)
-auto= Auto_player(net, "crazy_guy", frenzy=1)
-pickle.dump(auto, open("crazy_guy.p", "wb"))
