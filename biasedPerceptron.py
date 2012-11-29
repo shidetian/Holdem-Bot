76f34f006956ac84aa65a0e8a5399788c6ea05f1
ChorHizzle <cl676@cornell.edu>
2012-11-28 20:05:20 -0500

    Update biasedPerceptron.py
    Use it just like neural network, except the initialization should not have n_hidden, n_out and biased.

--- biasedPerceptron.py
+++ biasedPerceptron.py
@@ -1,1 +1,67 @@
-adfkaklfl;a
+import numpy as np
+
+def sigmoid(x, c=1):
+    return 1.0 / (1 + np.exp( -c * x ))
+
+def step(x):
+    if x >= 0: return 1
+    else: return 0
+
+def biased(x):
+    # add the entry 1 to the front of an list/array
+    return np.append( 1, x )
+    
+
+class BiasedPerceptron:
+    # same as Neural network, excpet no n_hidden, n_out and biased.
+    def __init__(self, n_in, n_out=1, alpha=0.5, lamb=1.0, randomInit=True):
+        # Size of the network
+        self.n_in = n_in
+        self.n_out = 1
+        # parameters
+        self.alpha = alpha
+        self.lamb = lamb
+        # Weight vectors
+        self.w = np.random.uniform(-.5, .5, (n_in + 1, n_out))
+
+    ###### Predicting
+    def predict(self, x):
+        # Given x, an array of n_in input,
+        # return a np.array of the hidden
+        return np.dot( np.transpose(self.w), biased(x) )
+
+
+    ############# supervised Learning
+    def learnOnline(self, x, y, alpha=.5):
+        '''
+        learning for neural network
+        '''
+        # hBiased is 2-dimensional: 1 by n_hidden+1
+        o = self.predict(x)
+        error = y - o
+        self.w += alpha * error * biased(x)[:, np.newaxis]
+
+    def learnTD(self, xLis, y):
+        '''
+        matrix implementation of learnTD
+        learning for 1 entire round, with x being a 2d array
+        and y be the payout at the end
+        '''
+        trace = np.zeros((self.n_in + 1, self.n_out))
+        w_change = np.zeros((self.n_in + 1, self.n_out))
+        for i in range(len(xLis)):
+            # n_in + 1
+            currX = xLis[i]
+            xBiased = biased( currX )[:,np.newaxis]
+            # n_out
+            currY = self.predict( currX )
+            if i < len(xLis) - 1:
+                nextY = self.predict( xLis[i+1] )
+                error = nextY - currY
+            else: # last update's reward is the payout y.
+                error = y - currY
+            trace *= self.lamb
+            trace += xBiased
+            w_change += self.alpha * error * trace
+            
+        self.w += w_change
