adf767d08960fdbf5d6340feba3f0167349bbe32
chorhizzle <cl676@cornell.edu>
2012-12-08 10:01:32 -0500

    using momentum=0.5 and only card info

--- frenzy_vs_frenzy.py
+++ frenzy_vs_frenzy.py
@@ -1,23 +1,26 @@
 import pickle
 import UnbiasedNet
 import anotherStatus as fw
-name = 'frenzy_vs_frenzy.p'
+from anotherStatusBots import CallingStation
+name = 'momentum_vs_calling_.005_.95_2e6.p'
 
-ALPHA = 0.005
-LAMB = 0.9
-n_train = 100000
+ALPHA = 0.002
+LAMB = .95
+n_train = 200000
 
 stat_obj = fw.AnotherStatus()
-n_cards = len(stat_obj.vec_cards)
+n_cards = sum( len(stat_obj.vec_cards[key]) for key in stat_obj.vec_cards )
+print n_cards
 
 net = UnbiasedNet.NeuralNet(fw.n_in, n_cards, fw.n_out,
-                           alpha=ALPHA, lamb=LAMB,
+                           alpha=ALPHA, lamb=LAMB, momentum=0.5,
                             subdiv=[(0,0), (n_cards,n_cards),
                                     (fw.n_in,n_cards)])
+net2 = UnbiasedNet.NeuralNet(fw.n_in, fw.n_hidden, fw.n_out,
+                                alpha=ALPHA, lamb=LAMB, randomInit=True)
 auto = fw.AnotherAutoPlayer(net, name="superbot")
-ai = fw.AnotherAutoPlayer(net, name='cpu', frenzy=1)
-
-auto.train(n_train, ai, debug=0, frenzy=1)
+ai = CallingStation()
+auto.train(n_train, ai, debug=0, frenzy=0)
 pickle.dump(auto, open(name, "wb"))
 
 auto = pickle.load(open(name, 'rb'))
