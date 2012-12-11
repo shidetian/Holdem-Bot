f45933e7597bb69c877f06e27fc52b9a713ff1d2
Detian Shi <hi_cannon@yahoo.com>
2012-12-11 00:05:27 -0500

    bots

--- realized_dealer_vs_betting.py
+++ realized_dealer_vs_betting.py
@@ -7,7 +7,7 @@ from betting_station import Betting_station
 ALPHA = 0.002
 n_train = 200000
 
-for LAMB in [0.7, 0.65, 0.6, 0.55, 0.5]:
+for LAMB in [0.65, 0.6, 0.55, 0.5]:
     name = 'realized_dealer_vs_betting_.002_' + str(LAMB) + '_2e6.p'
     stat_obj = fw.AnotherStatus()
     n_cards = sum( len(stat_obj.vec_cards[key]) for key in stat_obj.vec_cards )
@@ -32,5 +32,5 @@ for LAMB in [0.7, 0.65, 0.6, 0.55, 0.5]:
     data += '\n Results: ' + str(result)
     
     name = 'result_' + name[:-2] +  '.txt'
-    with open('name', 'w') as f:
+    with open(name, 'w') as f:
         f.write(data)
